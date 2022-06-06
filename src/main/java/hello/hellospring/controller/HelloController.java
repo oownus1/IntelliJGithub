e3efccb281c122925936df70012bc740964b0501
Sunwoo Park <oownus1@gmail.com>
2022-06-06 12:26:04 +0900

    commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,48 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody  //http(응답 바디부분)바디부분에 내가 직접 데이터를 넣어주겠다는 뜻
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); //json 방식?!
+        hello.setName(name);
+        return hello; //문자가 아닌 객체 넘기기
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
